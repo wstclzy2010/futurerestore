7c1ee50ff7a1478321858ad8463920f3cb3525b3
s0uthwest <pashok19967@yandex.ru>
2019-01-05 06:37:02 -0800

    SEP alert as a baseband

--- futurerestore/main.cpp
+++ futurerestore/main.cpp
@@ -99,7 +99,7 @@ int main(int argc, const char * argv[]) {
 
     int err=0;
     int res = -1;
-    printf("Version: " VERSION_COMMIT_SHA_FUTURERESTORE" - " VERSION_COMMIT_COUNT_FUTURERESTORE"\n");
+    printf("Version: " VERSION_COMMIT_SHA_FUTURERESTORE" - " VERSION_COMMIT_COUNT_FUTURERESTORE"\n"); // versioning
 #ifdef HAVE_LIBIPATCHER
     printf("%s\n",libipatcher::version().c_str());
     printf("Odysseus Support: yes\n");
@@ -247,7 +247,7 @@ int main(int argc, const char * argv[]) {
             devVals.deviceBoard = const_cast<char *>(device->hardware_model);
 
             if (flags & FLAG_LATEST_SEP) {
-                info("user specified to use latest signed sep\n");
+                info("user specified to use latest signed sep (WARNING, THIS CAN CAUSE A NON-WORKING RESTORE)\n");
                 client.loadLatestSep();
             } else if (!client.is32bit()) {
                 if (sourceIpswPath != nullptr) {
